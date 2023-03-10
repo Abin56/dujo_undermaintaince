51b18531a4d74b5823dd71165caa15944b24a158
Abin56 <abinjohn8089@gmail.com>
2023-03-10 11:14:45 +0530

    creating main adminpanel

--- lib/view/pages/home/create_school/create_school.dart
+++ lib/view/pages/home/create_school/create_school.dart
@@ -1,10 +1,8 @@
 import 'dart:math';
-
 import 'package:country_state_city_picker/country_state_city_picker.dart';
-import 'package:dujo_website/model/create_school_model/create_school_model.dart';
-import 'package:dujo_website/view/pages/web/widgets/custom_blue_button.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../model/schools_to_be_verified/schools_to_be_verified_create_list.dart';
 class SchoolProfile extends StatefulWidget {
   SchoolProfile({super.key});
 
@@ -21,7 +19,11 @@ class _SchoolProfileState extends State<SchoolProfile> {
 
   TextEditingController adminUserNameController = TextEditingController();
 
-  TextEditingController adminPasswordController = TextEditingController();
+  TextEditingController adminPasswordController = TextEditingController(); 
+
+  TextEditingController phoneNumberController = TextEditingController(); 
+
+  TextEditingController emailController = TextEditingController();
 
   String countryValue = '';
   String stateValue = '';
@@ -115,6 +117,28 @@ class _SchoolProfileState extends State<SchoolProfile> {
                     ),
                   ),
                 ),
+                 Padding(
+                  padding: EdgeInsets.all(15),
+                  child: TextField(
+                    controller: emailController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  ),
+                ),
+                 Padding(
+                  padding: EdgeInsets.all(15),
+                  child: TextField(
+                    controller: phoneNumberController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Phone Number',
+                    ),
+                  ),
+                ),
                 Container(
                   height: screenSize.width * 1 / 25,
                   width: screenSize.width * 1 / 7,
@@ -125,19 +149,29 @@ class _SchoolProfileState extends State<SchoolProfile> {
                         borderRadius: BorderRadius.circular(20),
                       ),
                     ),
-                    onPressed: () {
-                      final schoolDetails = CreatedSchoolAddModel(
-                          id: adminUserNameController.text.trim() + schoolID,
-                          schoolName: schoolNameController.text.trim(),
-                          schoolID: schoolNameController.text.substring(0,5) + schoolID,
-                          district: cityValue.toString(),
-                          place: placeController.text.trim(),
-                          adminUserName: adminUserNameController.text.trim(),
-                          password: adminPasswordController.text.trim(),
-                          postedDate: DateTime.now().toString());
+                    onPressed: () { 
+                      final schoolDetails = SchoolsToBeVerified
+                      (schoolName: schoolNameController.text, schoolID: schoolNameController.text.substring(0,5) + cityValue.substring(0,5) + schoolID ,
+                      id: adminUserNameController.text.trim()+ schoolID,
+                       district: cityValue.toString(),
+                        place: placeController.text.trim(), 
+                        adminUserName: adminUserNameController.text.trim(),
+                         password: adminPasswordController.text.trim(),
+                          phoneNumber: phoneNumberController.text, email: emailController.text, postedDate: DateTime.now().toString()); 
 
-                      CreateSchoolAddToFireBase()
-                          .createSchoolController(schoolDetails, context);
+                          AddRequestedSchoolsToFirebase().addRequestedSchools(schoolDetails, context);
+                      // final schoolDetails = CreatedSchoolAddModel(
+                      //     id: adminUserNameController.text.trim() + schoolID,
+                      //     schoolName: schoolNameController.text.trim(),
+                      //     schoolID: adminUserNameController.text.trim() + schoolID,
+                      //     district: cityValue.toString(),
+                      //     place: placeController.text.trim(),
+                      //     adminUserName: adminUserNameController.text.trim(),
+                      //     password: adminPasswordController.text.trim(),
+                      //     postedDate: DateTime.now().toString());
+
+                      // CreateSchoolAddToFireBase()
+                      //     .createSchoolController(schoolDetails, context);
                     },
                     child: Text("Create"),
                   ),
